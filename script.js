4b27b9f142aa4860134cb7f25b9a69442a2a3fa0
zahra Ardalan <z.ardalan78@gmail.com>
2024-12-07 15:19:40 +0330

    finish

--- script.js
+++ script.js
@@ -30,13 +30,58 @@ async function getweatherData(city) {
 }
 
 function displayWeatherInfo(data) {
-  const { name: city,main:{temp,humidity},weather:[{description,id}]} =data;
-  card.textContent="";
-  card.style.display ="flex";
- }
+  const {
+    name: city,
+    main: { temp, humidity },
+    weather: [{ description, id }],
+  } = data;
+  card.textContent = "";
+  card.style.display = "flex";
+  const cityDisplay = document.createElement("h1");
+  const tempDisplay = document.createElement("p");
+  const humidityDisplay = document.createElement("p");
+  const descDisplay = document.createElement("p");
+  const WeatherEmoji = document.createElement("p");
 
+  cityDisplay.textContent = city;
+  tempDisplay.textContent = `${(temp - 273.15).toFixed()}°c`;
+  humidityDisplay.textContent = `Humidity : ${humidity}%`;
+  descDisplay.textContent = description;
+  WeatherEmoji.textContent = getweatherEmoji(id);
 
-function getweatherEmoji(weatherId) {}
+  cityDisplay.classList.add("cityDisplay");
+  tempDisplay.classList.add("tempDisplay");
+  humidityDisplay.classList.add("humidityDisplay");
+  descDisplay.classList.add("descDisplay");
+  WeatherEmoji.classList.add("WeatherEmoji");
+
+  card.appendChild(cityDisplay);
+  card.appendChild(tempDisplay);
+  card.appendChild(humidityDisplay);
+  card.appendChild(descDisplay);
+  card.appendChild(WeatherEmoji);
+}
+
+function getweatherEmoji(weatherId) {
+  switch (true) {
+    case weatherId >= 200 && weatherId < 300:
+      return "🌧️";
+    case weatherId >= 300 && weatherId < 400:
+      return "🌩️";
+    case weatherId >= 500 && weatherId < 600:
+      return "🌧️";
+    case weatherId >= 600 && weatherId < 700:
+      return "🌨️❄️";
+    case weatherId >= 700 && weatherId < 800:
+      return "🌫️";
+    case weatherId === 800:
+      return "☀️";
+    case weatherId >= 801 && weatherId < 810:
+      return "☁️";
+    default:
+      return "❓";
+  }
+}
 function DisplayError(message) {
   const errorDisplay = document.createElement("p");
   errorDisplay.textContent = message;
